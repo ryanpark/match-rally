feba690829336e78535117776c1dcfe89df17287
Ryan P <ryan81.park@gmail.com>
2024-04-29 23:06:00 +1000

    auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -7,6 +8,10 @@ export const authOptions = {
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
   // session: {
